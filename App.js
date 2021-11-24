9156bb05d5bebecab44526874fb9d6cc53a62970
abrarfahim19 <fahimpatwari19@gmail.com>
2021-11-24 23:19:39 +0600

    Picker added

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import ViewImageScreen from './App/Screens/ViewImageScreen';
-import WelcomeScreen from './App/Screens/WelcomeScreen';
+import React, { useState } from "react";
+
+import Screen from "./app/components/Screen";
+import AppPicker from "./app/components/AppPicker";
+
+import { Button, View } from "react-native";
+import AppText from "./app/components/AppText";
+
+const categories1 = [
+  { label: "মগবাজার", value: 1 },
+  { label: "বনানী", value: 2 },
+  { label: "ধানমন্ডী", value: 3 },
+];
+
+const categories2 = [
+  { label: "মগবাজার", value: 1 },
+  { label: "বনানী", value: 2 },
+  { label: "ধানমন্ডী", value: 3 },
+];
 
 export default function App() {
+  const [category1, setCategory1] = useState('categories1[0]');
+
+  const [category2, setCategory2] = useState('categories2[0]');
+
   return (
-    <ViewImageScreen/>
-  )
+    <Screen>
+      <View style={{
+        width:'50%',
+        alignItems:'center',
+        left:'25%',
+    }}>
+        <AppText> From </AppText>
+        <AppPicker
+          selectedItem={category1}
+          onSelectItem={(item) => setCategory1(item)}
+          items={categories1}
+          icon="apps"
+          placeholder="Category"
+        />
+        <AppText> To </AppText>
+        <AppPicker
+          selectedItem={category2}
+          onSelectItem={(item) => setCategory2(item)}
+          items={categories2}
+          icon="apps"
+          placeholder="Category"
+        />
+        <Button title={'Go'}/>
+      </View>
+    </Screen>
+  );
 }
--- App/Screens/WelcomeScreen.js
+++ App/Screens/WelcomeScreen.js
@@ -1,44 +1,50 @@
-import React from 'react';
-import { ImageBackground, StyleSheet, View,Image, Text } from 'react-native';
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import AppButton from "../components/AppButton";
 
 function WelcomeScreen(props) {
-    return (
-        <ImageBackground style={styles.background} source={require('../assets/background.jpg')}>
-            <View style={styles.logoContainer}>
-                <Image style={styles.logo} source={require('../assets/logo-red.png')}/>
-                <Text>Sell What you don't Need</Text>
-            </View>
-            <View style={styles.login}></View>
-            <View style={styles.signup}></View>
-        </ImageBackground>
-    );
+  return (
+    <ImageBackground
+      blurRadius={10}
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <AppButton title="Login" />
+        <AppButton title="Register" color="secondary" />
+      </View>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-    background:{
-        flex:1,
-        justifyContent:'flex-end',
-        alignItems:'center',
-    },
-    login:{
-        height:50,
-        width:'100%',
-        backgroundColor:'#fc5c65'
-    },
-    logo:{
-        height:100,
-        width:100,
-    },
-    logoContainer:{
-        alignItems:'center',
-        position:'absolute',
-        top:50,
-    },
-    signup:{
-        height:50,
-        width:'100%',
-        backgroundColor:'#4ecdc4'
-    }
-})
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonsContainer: {
+    padding: 20,
+    width: "100%",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
+  },
+});
 
 export default WelcomeScreen;
